0415807acbdd085df4d8c9a3177645853fc1c2bb
Stephen Greene <sgreene@redhat.com>
2021-06-30 15:43:12 -0400

    externaldnscontroller: Implement `New(...)` function
    pkg/operator/controller/externaldns/controller.go:
    
    Implement a `New(...)` function for creating the externaldns
    controller with customizations.
    
    pkg/operator/operator.go:
    Update externaldns controller creation calls.
    
    Use a non-caching client, similair to that in the
    ingress and DNS operators.

--- pkg/operator/controller/externaldns/controller.go
+++ pkg/operator/controller/externaldns/controller.go
@@ -19,33 +19,64 @@ package externaldnscontroller
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"github.com/go-logr/logr"
 
 	operatorv1alpha1 "github.com/openshift/external-dns-operator/api/v1alpha1"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-// ExternalDNSReconciler reconciles a ExternalDNS object
-type ExternalDNSReconciler struct {
-	client.Client
-	Scheme            *runtime.Scheme
-	OperatorNamespace string
-	OperandNamespace  string
+const (
+	controllerName = "externaldns_controller"
+)
+
+// Config holds all the things necessary for the controller to run.
+type Config struct {
+	// Namespace is the namespace that ExternalDNS should be deployed in.
+	Namespace string
+	// Image is the ExternalDNS image to use.
+	Image string
 }
 
-func (r *ExternalDNSReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+// reconciler reconciles an ExternalDNS object.
+type reconciler struct {
+	config Config
+	client client.Client
+	log    logr.Logger
+}
+
+// New creates the externaldns controller from mgr and cfg. The controller will be pre-configured
+// to watch for ExternalDNS objects across all namespaces.
+func New(mgr manager.Manager, cfg Config) (controller.Controller, error) {
+	r := &reconciler{
+		config: cfg,
+		client: mgr.GetClient(),
+		log:    ctrl.Log.WithName(controllerName),
+	}
+
+	c, err := controller.New(controllerName, mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return nil, err
+	}
+	if err := c.Watch(&source.Kind{Type: &operatorv1alpha1.ExternalDNS{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// Reconcile reconciles watched objects and attempts to make the current state of
+// the object match the desired state.
+func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
 	// your logic here
 
 	return ctrl.Result{}, nil
-}
-
-// SetupWithManager sets up the controller with the Manager.
-func (r *ExternalDNSReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&operatorv1alpha1.ExternalDNS{}).
-		Complete(r)
 }
--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -67,19 +67,29 @@ func New(cliCfg *rest.Config, opCfg *operatorconfig.Config) (*Operator, error) {
 		NewCache: cache.MultiNamespacedCacheBuilder([]string{
 			opCfg.OperatorNamespace,
 			opCfg.OperandNamespace,
-		})}
+		}),
+		// Use a non-caching client everywhere. The default split client does not
+		// promise to invalidate the cache during writes (nor does it promise
+		// sequential create/get coherence), and we have code which (probably
+		// incorrectly) assumes a get immediately following a create/update will
+		// return the updated resource. All client consumers will need audited to
+		// ensure they are tolerant of stale data (or we need a cache or client that
+		// makes stronger coherence guarantees).
+		NewClient: func(_ cache.Cache, config *rest.Config, options client.Options, uncachedObjects ...client.Object) (client.Client, error) {
+			return client.New(config, options)
+		},
+	}
 
 	mgr, err := ctrl.NewManager(cliCfg, mgrOpts)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create manager: %w", err)
 	}
 
-	if err = (&externaldnscontroller.ExternalDNSReconciler{
-		Client:            mgr.GetClient(),
-		Scheme:            mgr.GetScheme(),
-		OperatorNamespace: opCfg.OperatorNamespace,
-		OperandNamespace:  opCfg.OperandNamespace,
-	}).SetupWithManager(mgr); err != nil {
+	// Create and register the externaldns controller with the operator manager.
+	if _, err := externaldnscontroller.New(mgr, externaldnscontroller.Config{
+		Namespace: opCfg.OperandNamespace,
+		Image:     opCfg.ExternalDNSImage,
+	}); err != nil {
 		return nil, fmt.Errorf("failed to create externaldns controller: %w", err)
 	}
 
