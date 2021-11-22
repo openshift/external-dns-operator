dd0a3717837ab5de7a52e8483bb5a20e7c8e54d1
Andrey Lebedev <alebedev@redhat.com>
2021-11-22 13:55:34 +0100

    e2e test: use unique hosted zone and custom resolver

---
+++ pkg/version/version.go
@@ -0,0 +1,8 @@
+package version
+
+var (
+	// COMMIT contains the hash of the VCS commit
+	COMMIT string
+	// SHORTCOMMIT contains the shorten version of the VCS commit's hash
+	SHORTCOMMIT string
+)
