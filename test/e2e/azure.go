1721ac6593ab0d52e38d60c2cf92a6a8746d8766
Nagarjuna Sarvepalli <snagarju@redhat.com>
2021-12-07 17:29:39 +0530

    Missed the code snip for GCP use case, due to porting

--- test/e2e/azure.go
+++ test/e2e/azure.go
@@ -38,8 +38,6 @@ type azureTestHelper struct {
 	zoneClient dns.ZonesClient
 }
 
-var _ providerTestHelper = &azureTestHelper{}
-
 // Build the necessary object for the provider test
 // for Azure Need the credentials ref clusterConfig
 func newAzureHelper(kubeClient client.Client) (providerTestHelper, error) {
@@ -131,8 +129,9 @@ func (a *azureTestHelper) deleteHostedZone(zoneID, zoneDomain string) error {
 	return nil
 }
 
-func (a *azureTestHelper) buildExternalDNS(name, zoneID, zoneDomain string, credsSecret *corev1.Secret) operatorv1alpha1.ExternalDNS {
-	resource := defaultExternalDNS(name, zoneID, zoneDomain)
+func (a *azureTestHelper) buildExternalDNS(name, zoneID, zoneDomain string, sourceType, routerName string,
+	credsSecret *corev1.Secret) operatorv1alpha1.ExternalDNS {
+	resource := defaultExternalDNS(name, zoneID, zoneDomain, sourceType, routerName)
 	resource.Spec.Provider = operatorv1alpha1.ExternalDNSProvider{
 		Type: operatorv1alpha1.ProviderTypeAzure,
 		Azure: &operatorv1alpha1.ExternalDNSAzureProviderOptions{
@@ -140,14 +139,6 @@ func (a *azureTestHelper) buildExternalDNS(name, zoneID, zoneDomain string, cred
 				Name: credsSecret.Name,
 			},
 		},
-	}
-	return resource
-}
-
-func (a *azureTestHelper) buildOpenShiftExternalDNS(name, zoneID, zoneDomain string) operatorv1alpha1.ExternalDNS {
-	resource := routeExternalDNS(name, zoneID, zoneDomain)
-	resource.Spec.Provider = operatorv1alpha1.ExternalDNSProvider{
-		Type: operatorv1alpha1.ProviderTypeAzure,
 	}
 	return resource
 }
