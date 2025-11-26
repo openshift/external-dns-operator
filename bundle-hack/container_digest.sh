d7a9986a02044fff9dc0fa22b527ea3f06b873f6
Andrey Lebedev <alebedev@redhat.com>
2025-11-26 21:42:04 +0100

    Konflux bundle: Update kube-rbac-proxy pullspec to latest v4.14
    This commit updates kube-rbac-proxy pullspec to the latest
    digest of `v4.14` floating tag.

--- bundle-hack/container_digest.sh
+++ bundle-hack/container_digest.sh
@@ -4,4 +4,6 @@ export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/external-dns-opera
 # Controller
 export OPERAND_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/external-dns-operator-tenant/ext-dns-optr-1-3-rhel-9/external-dns-container-ext-dns-optr-1-3-rhel-9@sha256:8a88bc8c4ad8e5cf2da8b003040a9d7e3e353e52982e3588b6539a5135763cb2'
 # kube-rbac-proxy
-export KUBE_RBAC_PROXY_IMAGE_PULLSPEC='registry.redhat.io/openshift4/ose-kube-rbac-proxy@sha256:56653df97e4498ecd67b9645d75e44f65c625594e312dbcf0f09937b57d76010'
+# Latest version of v4.14 tag is used.
+# Catalog link (health grade A): https://catalog.redhat.com/en/software/containers/openshift4/ose-kube-rbac-proxy/5cdb2634dd19c778293b4d98?image=691eb72e6d4c48dbffa76548
+export KUBE_RBAC_PROXY_IMAGE_PULLSPEC='registry.redhat.io/openshift4/ose-kube-rbac-proxy@sha256:ba9ff4c933739f1774bc8277d636053c5306863221a8c7b7b9ddc4470eb7feff'
