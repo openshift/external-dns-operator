9adc4965971dcdf5fc9f329cc8a50db2bc3bbab1
Ali Syed <thealisyed@gmail.com>
2026-05-25 16:19:24 +0100

    NE-2730: Update kube-rbac-proxy to the latest rhel8 based v4.15
    Updates kube-rbac-proxy pullspec to the latest digest of the v4.15
    floating tag. The rhel8 ose-kube-rbac-proxy is only available up to
    v4.15 (v4.17+ moved to rhel9). This brings the health grade from D
    to B, which is the best available for rhel8.
    
    Assisted with Claude.

--- bundle-hack/container_digest.sh
+++ bundle-hack/container_digest.sh
@@ -4,6 +4,6 @@ export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/edo/external-dns-rhel8-operat
 # Controller
 export OPERAND_IMAGE_PULLSPEC='registry.redhat.io/edo/external-dns-rhel8@sha256:65b8adf165bf8ad7fabcc8915f63e3d99bc172f8b9441fcdea5b9856b73ea980'
 # kube-rbac-proxy
-# Latest version of v4.14 tag is used.
-# Catalog link (health grade A): https://catalog.redhat.com/en/software/containers/openshift4/ose-kube-rbac-proxy/5cdb2634dd19c778293b4d98?image=691eb72e6d4c48dbffa76548
-export KUBE_RBAC_PROXY_IMAGE_PULLSPEC='registry.redhat.io/openshift4/ose-kube-rbac-proxy@sha256:ba9ff4c933739f1774bc8277d636053c5306863221a8c7b7b9ddc4470eb7feff'
+# Latest version of v4.15 tag is used.
+# Catalog link (health grade B): https://catalog.redhat.com/en/software/containers/openshift4/ose-kube-rbac-proxy/5cdb2634dd19c778293b4d98?image=6a047feb2defcf172d2f13ab
+export KUBE_RBAC_PROXY_IMAGE_PULLSPEC='registry.redhat.io/openshift4/ose-kube-rbac-proxy@sha256:814e0ec7d531113a01b327a1f8719e4d42ec4b6683b96728c5bcfab4a3a4ebcf'
